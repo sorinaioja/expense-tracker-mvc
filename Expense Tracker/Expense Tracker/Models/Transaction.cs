d204950e786e3981aaec8912df5c72712336465b
sorinaioja <sorina_ioja@yahoo.com>
2024-02-18 20:01:32 +0200

    charts and dashboard

--- Expense Tracker/Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Expense Tracker/Models/Transaction.cs
@@ -1,12 +1,34 @@
-﻿namespace Expense_Tracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
 {
     public class Transaction
     {
         public int Id { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public double Amount { get; set; }
         public string? Note { get; set; }
         public DateTime Date { get; set; }
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
     }
 }
